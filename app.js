8ddc0e9305e0f79c8dbe87f8ddadff8ba466b20d
ZeSved <timothy.korell@gmail.com>
2022-08-03 18:21:31 +0200

    Refacted code added dragging code added clear

--- app.js
+++ app.js
@@ -1,33 +1,82 @@
 const colorContainer = document.querySelector(".color-container")
 const canvasGrid = document.querySelector(".canvas-grid")
+const clearButton = document.querySelector('.clear')
 
-colorContainer.addEventListener("click", (e) => {
-    const clickedDiv = e.target
+const BACKGROUND_COLOR = "#777"
 
-    if (clickedDiv.classList.value === "color-container") return
-    
-    applySelectedOutLine(clickedDiv)
-})
+setupEventListeners()
+function setupEventListeners(){
+    canvasGrid.addEventListener("click", (e) => {
+        const clickedPixel = e.target
+        
+        const color = processInteraction(clickedPixel)
+        if(!color) return
+        
+        if(clickedPixel.classList.value === "canvas-grid") return
+        
+        paint(clickedPixel, color)
+    })
+    canvasGrid.addEventListener("contextmenu", (e) => {
+        e.preventDefault()
+        const clickedPixel = e.target
+        
+        const color = processInteraction(clickedPixel)
+        if(!color) return
+        
+        erase(clickedPixel)
+    })
+    canvasGrid.addEventListener("mousemove", (e) => {
+        const clickedPixel = e.target
+        
+        const color = processInteraction(clickedPixel)
+        if(!color) return
+        
+        if(e.buttons === 1){
+            paint(clickedPixel, color)
+        } else if (e.buttons === 2){
+            erase(clickedPixel)
+        }
+    })
+    colorContainer.addEventListener("click", (e) => {
+        const clickedDiv = e.target
+        
+        if (clickedDiv.classList.value === "color-container") return
+        
+        applySelectedOutLine(clickedDiv)
+    })
+    clearButton.addEventListener('click', clear)
+}
 
+// Process functions
 function applySelectedOutLine(div) {
     const selectedElms = colorContainer.querySelectorAll(".selected")
-
+    
     selectedElms.forEach((elm) => {
         elm.classList.remove("selected")
     })
-
+    
     div.classList.add("selected")
 }
 
-canvasGrid.addEventListener("click", (e) => {
+function processInteraction (elm) {
     const selectedColor = colorContainer.querySelector(".selected")
-
-    if (!selectedColor) return
+    if (!selectedColor || elm.classList.value === "canvas-grid") return
     
-    const color = window.getComputedStyle(selectedColor).backgroundColor
-    const clickedPixel = e.target
+    return window.getComputedStyle(selectedColor).backgroundColor
+}
 
-    if(clickedPixel.classList.value === "canvas-grid") return
-    
-    clickedPixel.style.backgroundColor = color
-})
+// Action functions
+function paint(elm, color) {
+    elm.style.backgroundColor = color
+}
+
+function erase(elm) {
+    elm.style.backgroundColor = BACKGROUND_COLOR
+}
+
+function clear() {
+    const pixels = canvasGrid.children
+    for (let i = 0; i < pixels.length; i++) {
+        erase(pixels[i])
+    }
+}
